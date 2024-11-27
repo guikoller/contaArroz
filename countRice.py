1968a5a9e7db03ba10d989123477dfe0763f6a05
Mauricio Höger <mayarahoger@gmail.com>
2024-11-27 13:08:59 -0300

    diminuicao do filtro da mediana, calculo da diferenca do desvio padrao para identificar somente os arroz individuais

--- countRice.py
+++ countRice.py
@@ -42,67 +42,56 @@ def rotula (img):
 
 def estimate_blob_size(components):
     sizes = sorted(componente['n_pixels'] for componente in components)  
-  
-    list_diff = []
-    for i in range(len(sizes)-1):
-        list_diff.append(round(sizes[i+1] - sizes[i]))
+    #print(sizes)
     
-    print(sizes)
+    desvio = []  
+    single_list = sizes
+    for i in reversed(range(3, len(sizes))):
+        desvio.insert(0, float(round(abs(np.std(sizes[:i]) - np.std(sizes[:i-1])), 2))) #diferença dos desvios
 
-    for i in range(len(sizes)-1):
-        diff = sizes[i+1] - sizes[i]
-        single_list = sizes
-        if diff > 200:
+    #print(desvio)
+
+    for i in range(round(0.1*len(sizes)), len(desvio)):
+        if desvio[i] > 10: # diferença entre dois desvios é maior do que 10, significa que tem um grupo de arroz na lista de arroz
             single_list = sizes[:i]
-            print(i)
+            #print(i)
             break
-        
-            # o menor valor tem que ser mais ou menos a metade do tamanho do arroz mediano
-            # o valor mediano tem que ser no máximo 2,5 vezes maior que o menor valor
-            # desconsiderar os outros menores valores
-
-    for i in range(len(single_list)-1):
-        if np.median(single_list)/single_list[i] < 2: #esse é o local de corte
-            print(np.median(single_list[i:]))
-            return np.median(single_list[i:])
     
-    return np.median(sizes)
-    lower_third = sizes[:len(sizes) // 3]
-    return sum(lower_third) / len(lower_third)
+    #print(np.median(single_list))
+    return np.median(single_list)
 
 def estimate_quantity(components, avg_blob_size):
     estimated_quantity = 0
     for component in components:
         if component['n_pixels'] > avg_blob_size:
-            estimated_quantity += math.floor(component['n_pixels'] / avg_blob_size)
-            if round(component['n_pixels'] / avg_blob_size) != 1.0:
-                #print((component['n_pixels'] / avg_blob_size))
-                pass
+            #estimated_quantity += math.floor(component['n_pixels'] / avg_blob_size)
+            estimated_quantity += round(component['n_pixels'] / avg_blob_size)
         else:
             estimated_quantity += 1
     return estimated_quantity
 
 def countRice(img, filename): 
-    img = cv2.medianBlur(img, 11)
-    # cv2.imshow("median/"+filename, img) 
+    img = cv2.medianBlur(img, 5)
+    #cv2.imshow("median/"+filename, img) 
     img = cv2.GaussianBlur(img, (7, 7), 0)
-    # cv2.imshow("gaussian/"+filename, img)
+    #cv2.imshow("gaussian/"+filename, img)
+
     img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-    # cv2.imshow("normalize/"+filename, img)
+    #cv2.imshow("normalize/"+filename, img)
     img = cv2.Canny(img, 50, 150)
-    # cv2.imshow("canny/"+filename, img)
+    #cv2.imshow("canny/"+filename, img)
     
     kernel = np.array([[0, 1, 0], 
                        [1, 1, 1], 
                        [0, 1, 0]], dtype=np.uint8)
     img = cv2.dilate(img, kernel, iterations=1)
-    # cv2.imshow("dilated/"+filename, img)
+    #cv2.imshow("dilated/"+filename, img)
     
     contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         cv2.drawContours(img, [contour], -1, (255, 255, 255), thickness=cv2.FILLED)
-    cv2.imshow("flooded/"+filename, img)
-
+    #cv2.imshow("flooded/"+filename, img)
+    
     img = img.astype (np.float32)/ 255.0
     components = rotula(img)
     quantity = estimate_quantity(components, estimate_blob_size(components))
